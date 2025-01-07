a13f6f1b830dd572aad4ee35c81660a09a88f2bc
hook <jadepureone@outlook.com>
2025-01-07 18:35:54 +0800

    🎉 init(UserView): delete edit
    mind edit uses's binding of command and commadParam
    
    https://www.bilibili.com/cheese/play/ep1375567?csource=common_myclass_purchasedlecture_null&spm_id_from=333.874.selfDef.mine_paid_list

--- com.myxmu.SCADASystem/ViewModels/UserViewModel.cs
+++ com.myxmu.SCADASystem/ViewModels/UserViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using System.Windows;
+using AngleSharp.Dom;
 using com.myxmu.SCADASystem.Models;
 using com.myxmu.SCADASystem.Services;
 using com.myxmu.SCADASystem.Views;
 using Common.Helpers;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Masuit.Tools;
 using MaterialDesignThemes.Wpf;
 
 namespace com.myxmu.SCADASystem.ViewModels
@@ -21,15 +23,33 @@ namespace com.myxmu.SCADASystem.ViewModels
             UserSession = userSession;
         }
 
+
+        /// <summary>
+        /// 是否有管理员权限
+        /// </summary>
+        /// <returns></returns>
+        private bool HasPermiss()
+        {
+            if (UserSession.CurrentUser.Role == 0)
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("no permission");
+                return false;
+
+            }
+        }
+
         /// <summary>
         /// 添加用户
         /// </summary>
         [RelayCommand]
         private async void AddUser(UserViewModel user)
         {
-            if (UserSession.CurrentUser.Role != 0)
+            if (!HasPermiss())
             {
-                MessageBox.Show("no permission");
                 return;
             }
 
@@ -55,16 +75,73 @@ namespace com.myxmu.SCADASystem.ViewModels
         /// </summary>
         /// <param name="user"></param>
         [RelayCommand]
-        private void DeleteUser(UserViewModel user)
+        private void DeleteUser(UserModel user)
         {
+            if (!HasPermiss())
+            {
+                return;
+            }
+            if(UserSession.CurrentUser.UserName == user.UserName)
+            {
+                MessageBox.Show("不能删除自己");
+                return;
+            }
+
+            var res = MessageBox.Show("确定要删除吗", "警告", MessageBoxButton.OKCancel);
+            if (res == MessageBoxResult.OK)
+            {
+                int count = SqlSugarHelper.Db.Deleteable<UserModel>().Where(e => e.Id == user.Id).ExecuteCommand();
+                if (count > 0)
+                {
+                    MessageBox.Show("Delete success");
+                    UserList = QueryTable();
+
+                }
+            }
         }
 
         /// <summary>
         /// 修改用户
         /// </summary>
         [RelayCommand]
-        private void ModifyUser(UserViewModel user)
+        private async void EditUser(UserModel user)
         {
+            if (!HasPermiss())
+            {
+                return;
+            }
+
+            //在对话框中，你实际上是在操作这个副本，而不是原始对象。这样可以确保原始数据不受影响，直到用户确认修改。
+            var userTemplate = user.DeepClone();
+
+            var view = new UserOperateDialogView()
+            {
+                DataContext = new UpdateViewModel<UserModel>(userTemplate)
+            };
+            var result = (bool)await DialogHost.Show(view, "ShellDialog");
+            if (result)
+            {
+                //Console.WriteLine($"Name:{entity.UserName}");
+
+                user.UserName = userTemplate.UserName;
+                user.Password = userTemplate.Password;
+                user.Role = userTemplate.Role;
+                user.UpDateTime = DateTime.Now;
+                
+                int count = await SqlSugarHelper.Db.Updateable<UserModel>(user).ExecuteCommandAsync();
+
+                if (count > 0)
+                {
+                    UserList = QueryTable();
+                    MessageBox.Show("edit success");
+                }
+                else
+                {
+                    MessageBox.Show("edit fail");
+
+                }
+
+            }
         }
 
         /// <summary>
