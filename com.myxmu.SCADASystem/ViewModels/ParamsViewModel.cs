78749c69143a17f2a9dd4955493062ac291ee01d
hook <jadepureone@outlook.com>
2025-01-16 15:18:55 +0800

    ✨ feat(view): Params UI，binding

--- com.myxmu.SCADASystem/ViewModels/ParamsViewModel.cs
+++ com.myxmu.SCADASystem/ViewModels/ParamsViewModel.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using com.myxmu.SCADASystem.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Extensions.Options;
 
 namespace com.myxmu.SCADASystem.ViewModels
 {
-    class ParamsViewModel
+    public partial class ParamsViewModel:ObservableObject
     {
+        [ObservableProperty] private AppSetting appSetting;
+
+        public ParamsViewModel(IOptionsSnapshot<AppSetting> optionsSnapshot)
+        {
+            this.appSetting = optionsSnapshot.Value;
+        }
     }
 }
