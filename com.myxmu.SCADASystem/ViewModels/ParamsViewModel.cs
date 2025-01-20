d54cf619b2f911275897c6aa73083de02a9149fa
hook <jadepureone@outlook.com>
2025-01-20 11:05:04 +0800

    ✨ feat(paramsViewMode;): automock

--- com.myxmu.SCADASystem/ViewModels/ParamsViewModel.cs
+++ com.myxmu.SCADASystem/ViewModels/ParamsViewModel.cs
@@ -14,6 +14,7 @@ namespace com.myxmu.SCADASystem.ViewModels
     public partial class ParamsViewModel:ObservableObject
     {
         private GlobalConfig _globalConfig;
+        private CancellationTokenSource _cts = new();
 
         [ObservableProperty] private AppSetting appSetting;
 
@@ -37,6 +38,82 @@ namespace com.myxmu.SCADASystem.ViewModels
                 _globalConfig.StopCollection();
             }
             
+        }
+
+
+        [RelayCommand]
+        void ToggleMock()
+        {
+            if (AppSetting.PlcParam.AutoMock)
+            {
+                StartMockData();
+            }
+            else
+            {
+                StopMockData();
+            }
+        }
+
+        private void StartMockData()
+        {
+            _cts = new CancellationTokenSource();
+
+            Task.Run(async () => {
+                var scadaData = new ScadaReadDataModel ();
+                // 获取所有浮点型的属性
+                var properties = typeof(ScadaReadDataModel).GetProperties()
+                .Where(p => p.PropertyType == typeof(float));
+                // 获取 Bool 值
+                var propertiesBool = typeof(ScadaReadDataModel).GetProperties()
+                    .Where(p => p.PropertyType == typeof(bool));
+
+                while (!_cts.Token.IsCancellationRequested)
+                {
+                    var random = new Random();
+
+                    foreach (var property in properties)
+                    {
+                        var value = (float)(random.NextDouble() * 4 + 1);
+
+                        var address = _globalConfig.ReadEntities.FirstOrDefault(
+                            x => x.En == property.Name)?.Address;
+
+                        if (!string.IsNullOrEmpty(address))
+                        {
+                            await _globalConfig.Plc.WriteAsync(address, value);
+                        }
+                    }
+
+                    foreach (var property in propertiesBool)
+                    {
+                        var address = _globalConfig.ReadEntities.FirstOrDefault(
+                            x => x.En == property.Name)?.Address;
+
+                        if (!string.IsNullOrEmpty(address))
+                        {
+                            if (random.Next(0, 2) != 0)
+                            {
+                                await _globalConfig.Plc.WriteAsync(address, true);
+                            }
+                            else
+                            {
+                                await _globalConfig.Plc.WriteAsync(address, false);
+                            }
+                        }
+                    }
+
+                    await Task.Delay(1000);
+                }
+            }, _cts.Token);
+        }
+
+        private void StopMockData()
+        {
+            if (_cts != null)
+            {
+                _cts.Cancel();
+                _cts.Dispose();
+            }
         }
     }
 }
