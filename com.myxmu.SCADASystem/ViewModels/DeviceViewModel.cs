615e09d5887f546f5b8d2a6a45a0c000c9b3c2b3
hook <jadepureone@outlook.com>
2025-01-20 11:39:54 +0800

    ✨ feat(DeviceView): 设备总控通讯

--- com.myxmu.SCADASystem/ViewModels/DeviceViewModel.cs
+++ com.myxmu.SCADASystem/ViewModels/DeviceViewModel.cs
@@ -3,11 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using com.myxmu.SCADASystem.Services;
+using Common.Helpers;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Model;
 
 namespace com.myxmu.SCADASystem.ViewModels
 {
     public partial class DeviceViewModel:ObservableObject
     {
+
+        [ObservableProperty]
+        private ScadaReadDataModel _scadaReadDataModel = new();
+
+        public readonly GlobalConfig GlobalConfig;
+        public readonly UserSession userSession;
+
+        public DeviceViewModel(GlobalConfig globalConfig, UserSession userSession)
+        {
+            GlobalConfig = globalConfig;
+            this.userSession = userSession;
+        }
+
+        [RelayCommand]
+        private void WriteDeviceControl(string paramsName)
+        {
+            if (!GlobalConfig.PlcConnected)
+            {
+                userSession.ShowMessageBox("PLC未连接或连接异常");
+                return;
+            }
+
+            var readAddress=GlobalConfig.ReadEntities.FirstOrDefault(x => x.En == paramsName)?.Address;
+
+            if (string.IsNullOrEmpty(readAddress)) {
+                userSession.ShowMessageBox("找不到对应读取地址");
+                return;
+            }
+
+            //写入PLC
+            var res=GlobalConfig.Plc.Write(readAddress,true );
+
+            if (res.IsSuccess) {
+                //userSession.ShowMessageBox("指令执行成功");
+            }
+
+        }
     }
 }
