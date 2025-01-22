6b43f5a6b2b670fa42e6517671cb3cc238df8d58
hook <jadepureone@outlook.com>
2025-01-22 14:15:24 +0800

    ✨ feat(FormulaVIew): ItemSource implent
    配方列表数据展示

--- com.myxmu.SCADASystem/ViewModels/FormulaViewModel.cs
+++ com.myxmu.SCADASystem/ViewModels/FormulaViewModel.cs
@@ -1,7 +1,10 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Common.Helpers;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +13,17 @@ namespace com.myxmu.SCADASystem.ViewModels
 {
     public partial class FormulaViewModel:ObservableObject
     {
+        [ObservableProperty]
+        ObservableCollection<FormulaEntity> _formulaList = new();
 
         [RelayCommand]
-        void QueryFormula() { }
+        void QueryFormula() {
+            FormulaList.Clear();
+            SqlSugarHelper.Db.Queryable<FormulaEntity>()
+                    .OrderBy(f => f.CreateDateTime, SqlSugar.OrderByType.Desc)
+                    .ToList()
+                    .ForEach(e=>FormulaList.Add(e));
+        }
 
         [RelayCommand]
         void NewFormulaCommand() { }
